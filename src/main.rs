a14cb572a780a8c6febc26652240e6e24e9a92ab
BiswajitThakur <biswajit733127@gmail.com>
2024-12-31 07:29:13 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
-use std::net::TcpListener;
+use std::{
+    io::{self, Write},
+    net::TcpListener,
+};
 
-fn main() {
+fn main() -> io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                stream.write_all(b"\x00\x00\x00\x00")?;
+                stream.write_all(b"\x00\x00\x00\x07")?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+    Ok(())
 }
